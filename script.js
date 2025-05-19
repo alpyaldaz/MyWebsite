e1c33c3c83b929d1ea832a7692b3f6556c44c347
Alp <ayaldaz@edu.cdv.pl>
2025-05-19 15:49:34 +0200

    Update Project Section, Add car animation landing page

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function openModal(filePath = './assests') {
+  document.getElementById("readmeModal").style.display = "block";
+  fetch(filePath)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("readme-text").innerHTML = marked.parse(data);
+    })
+    .catch(error => {
+      document.getElementById("readme-text").textContent = "Error loading README.";
+      console.error(error);
+    });
+}
+
+function closeModal() {
+  document.getElementById("readmeModal").style.display = "none";
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+  
+});
+
